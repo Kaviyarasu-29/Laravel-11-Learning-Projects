484c9db0777b69420be820ff0b4082a9a99bb241
Kaviyarasu-29 <kaviyarasu93458@gmail.com>
2024-06-28 11:31:29 +0530

    Routr Parametr Passing

--- Employee-Project/routes/web.php
+++ Employee-Project/routes/web.php
@@ -8,5 +8,27 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', function () {
-    return ("hai");
+    return config('app.url');
+});
+
+
+// Route Parameter
+
+// 1. ID parametr
+Route::get('/new/{id}', function ($id) {
+    return 'User New ID: ' . $id;
+});
+
+// 2. Name Parameter with Default Value
+Route::get('/user/{name?}', function ($name = 'AWP') {
+    return 'User Name: ' . $name;
+});
+
+// 3. Parameter to Render View or Return Default Message
+Route::get('/try/{name?}', function ($name = 'Guest') {
+    if ($name != 'Guest') {
+        return view($name);
+    } else {
+        return 'User Name: ' . $name;
+    }
 });
