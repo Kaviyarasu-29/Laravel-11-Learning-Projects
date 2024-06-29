d3b1ec09a522476b4b21f3d29125ed570a45e54c
Kaviyarasu-29 <kaviyarasu93458@gmail.com>
2024-06-29 11:51:02 +0530

    route basic Laravel-11 website

---
+++ Laravel-11/routes/web.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::redirect('/', '/try/{name?}');
+
+// I - Route Parameter
+// 1. ID parametr
+Route::get('/new/{id}', function ($id) {
+    return 'User New ID: ' . $id;
+});
+
+// 2. Name Parameter with Default Value
+Route::get('/user/{name?}', function ($name = 'AWP') {
+    return 'User Name: ' . $name;
+});
+
+// 3. Parameter to Render View or Return Default Message
+Route::get('/try/{name?}', function ($name = 'Guest') {
+    if ($name != 'Guest') {
+        return view($name);
+    } else {
+        return 'User Name: ' . $name;
+        // return view('welcome');
+    }
+});
+
+
+// II - Naming Router
+
+Route::get('/About',function(){
+    if (true){
+        return redirect()->route('About-page');
+    }
+});
+
+Route::get('/Employee-Project/Settings/About ', function(){
+    return "Naming route check";
+})->name('About-page');
+
+
+
+// view routes
+Route::view('/welcome', 'welcome', ['name' => 'Taylor']);
