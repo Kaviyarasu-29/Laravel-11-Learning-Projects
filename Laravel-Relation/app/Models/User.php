f3f3bc6f57d1f197320f5923bec409e89270f490
Kaviyarasu-29 <kaviyarasu93458@gmail.com>
2024-07-06 13:33:36 +0530

    hasOne data access using foreignkey

--- Laravel-Relation/app/Models/User.php
+++ Laravel-Relation/app/Models/User.php
@@ -4,8 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Phone;
 
 class User extends Model
 {
     use HasFactory;
+
+    public function phone(){
+        return $this->hasOne(Phone::class);
+    }
 }
--- Laravel-Relation/routes/web.php
+++ Laravel-Relation/routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('/',function() {
+    $user = User::find(1);
+    dd($user->Phone);
+} );
