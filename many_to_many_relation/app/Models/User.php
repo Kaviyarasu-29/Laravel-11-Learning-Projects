d5e1064d4bd27779cbcf28f67c7e7eb786fbe0e4
Kaviyarasu-29 <kaviyarasu93458@gmail.com>
2024-07-11 18:02:47 +0530

    many to many relation - model and migration created

---
+++ many_to_many_relation/app/Models/User.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class User extends Model
+{
+    use HasFactory;
+    /**
+     * The roles that belong to the user.
+     */
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(Role::class);
+    }
+}
