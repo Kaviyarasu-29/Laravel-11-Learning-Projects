07a9e15f633ec11576ff8b96f394f6824fd836b4
Kaviyarasu-29 <kaviyarasu93458@gmail.com>
2024-07-12 11:28:54 +0530

    many-to-many

--- many_to_many_relation/app/Models/User.php
+++ many_to_many_relation/app/Models/User.php
@@ -14,6 +14,6 @@ class User extends Model
      */
     public function roles(): BelongsToMany
     {
-        return $this->belongsToMany(Role::class);
+        return $this->belongsToMany(Role::class,UserRole::class);
     }
 }
--- many_to_many_relation/routes/web.php
+++ many_to_many_relation/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Models\User;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
 Route::get('/', function () {
-    return view('welcome');
+    $user = User::with('roles')->whereId(1)->first();
+    return Response::json($user);
 });
